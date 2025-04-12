825913e768d20376e39458f2a0c8f3130c112dd0
Nima Shadab <s309670@studenti.polito.it>
2025-04-12 15:32:08 +0200

    commit

---
+++ musicProject.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template, make_response
+
+app = Flask(__name__)
+_UPLOADED_ = 0
+@app.route("/")
+def hello_world():
+    return render_template('project_template.html')
+
+
+@app.route("/post/", methods=["POST"])
+def uploadedVideo():
+    global _UPLOADED_
+    print(_UPLOADED_)
+    if _UPLOADED_ == 1:
+        mr = make_response(render_template("project_template.html"), 404)
+        mr.headers["res"] = "You've already uploaded a file!"
+        return mr
+    else:
+        print(request.files.get("file").filename)
+        _UPLOADED_ = 1
+    return render_template("project_template.html")
+
+
+
