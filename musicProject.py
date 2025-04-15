a9adc0289bb4ab9ebbd97091fc1a99a86f060f27
Nima Shadab <s309670@studenti.polito.it>
2025-04-15 19:58:55 +0200

    added delete functionality -- server side

--- musicProject.py
+++ musicProject.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, render_template, make_response
+import os
+
 
 app = Flask(__name__)
 _UPLOADED_ = 0
+_FILE_NAME_ = ""
+
 @app.route("/")
 def hello_world():
     return render_template('project_template.html')
@@ -9,27 +13,31 @@ def hello_world():
 
 @app.route("/post/", methods=["POST"])
 def uploadedVideo():
-    global _UPLOADED_
-    print(_UPLOADED_)
+    global _UPLOADED_, _FILE_NAME_
     if _UPLOADED_ == 1:
         mr = make_response(render_template("project_template.html"), 404)
         mr.headers["res"] = "You've already uploaded a file!"
         return mr
     else:
-        print(request.files.get("file").filename)
+        request.files["file"].save(request.files["file"].filename)
         _UPLOADED_ = 1
+        _FILE_NAME_ = request.files["file"].filename
     return render_template("project_template.html")
 
 
 
 @app.route("/delete/", methods=["DELETE"])
 def deletedVideo():
-    global _UPLOADED_
+    global _UPLOADED_, _FILE_NAME_
     if _UPLOADED_ == 1:
         _UPLOADED_ = 0
+        os.remove(_FILE_NAME_)
+        _FILE_NAME_ = ""        
         mr = make_response(render_template("project_template.html"), 204)
         mr.headers["res"] = "file deleted!"
         return mr
+    else: 
+        return render_template('project_template.html')
 
 
 
